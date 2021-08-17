febf28337ab2a11243ac65fb6abd2aa8a5d94e98
ahsan.jawaid <ahsan.jawaid@systemsltd.com>
2021-08-17 12:27:15 +0500

    Test Case C301 has been completed

--- src/test/java/com/shoebacca/StepsDefinitions/HarnessVariables.java
+++ src/test/java/com/shoebacca/StepsDefinitions/HarnessVariables.java
@@ -18,6 +18,7 @@ public class HarnessVariables extends UtilFactory {
     protected static String PRODUCT_BRAND;
     protected static String PREVIOUS_PRODUCT_NAME;
     protected final String VALID_USERNAME = GlobalEnumVariables.VALID_LOGIN_USERNAME.getValue();
+    protected final String VALID_DATA_USERNAME = GlobalEnumVariables.VALID_LOGIN_DATA_USERNAME.getValue();
     protected final String INVALID_USERNAME = GlobalEnumVariables.INVALID_LOGIN_USERNAME.getValue();
     protected final String VALID_PASSWORD = GlobalEnumVariables.VALID_LOGIN_PASSWORD.getValue();
     protected final String INVALID_PASSWORD = GlobalEnumVariables.INVALID_LOGIN_PASSWORD.getValue();
