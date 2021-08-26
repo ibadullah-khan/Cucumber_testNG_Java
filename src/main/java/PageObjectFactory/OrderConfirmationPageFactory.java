886f74f663dc2a5e2faca5be20c391edb944f1bd
ZainAliQureshi <zain.ali.qureshi@hotmail.com>
2021-08-26 15:29:12 +0500

    C225072 Commit

--- src/main/java/PageObjectFactory/OrderConfirmationPageFactory.java
+++ src/main/java/PageObjectFactory/OrderConfirmationPageFactory.java
@@ -1,8 +1,10 @@
 package PageObjectFactory;
 
+import EnumFactory.CheckoutPageEnum;
 import UtilitiesFactory.UtilFactory;
 import EnumFactory.OrderConfirmationPageEnum;
 import com.aventstack.extentreports.Status;
+import org.openqa.selenium.NoSuchElementException;
 
 public class OrderConfirmationPageFactory extends UtilFactory {
 
@@ -64,6 +66,46 @@ public class OrderConfirmationPageFactory extends UtilFactory {
         catch (Exception e) {
             failureException = e.toString();
             scenarioDef.log(Status.FAIL,"Could not Click Create Account Button on Order Confirmation Page");
+            throw e;
+        }
+    }
+
+    public void clickPrintSlip() throws Exception {
+
+        String locator = OrderConfirmationPageEnum.XPATH_PRINT_SLIP.getValue();
+        try {
+            waitFactory.waitForElementToBeVisible(locator);
+            waitFactory.waitForElementToBeClickable(locator);
+            click(locator);
+            scenarioDef.log(Status.PASS,"Clicked Print Slip Button on Order Confirmation Page");
+        }
+        catch (Exception e) {
+            failureException = e.toString();
+            scenarioDef.log(Status.FAIL,"Could not Click Print Slip Button on Order Confirmation Page");
+            throw e;
+        }
+    }
+
+    public void validatePrintButtonVisibility(boolean expectedVisibility) {
+        String locator = OrderConfirmationPageEnum.XPATH_PRINT_BUTTON.getValue();
+        String errorMsg = null;
+        Boolean actualVisibility;
+        try {
+            actualVisibility = isVisible(locator);
+            if (actualVisibility && expectedVisibility) {
+                scenarioDef.log(Status.PASS, "Validated Print Button is Displayed as Expected on Checkout Page");
+            } else if (!actualVisibility && !expectedVisibility) {
+                scenarioDef.log(Status.PASS, "Validated Print Button is not Displayed as Expected on Checkout Page");
+            } else if (actualVisibility && !expectedVisibility) {
+                errorMsg = "Validated Print Button is Displayed Unexpected on Checkout Page";
+                throw new NoSuchElementException("Element Visibility was Unexpected for Element: " + locator);
+            } else if (!actualVisibility && expectedVisibility) {
+                errorMsg = "Validated Print Button is Displayed Unexpectedly on Checkout Page";
+                throw new NoSuchElementException("Element Visibility was Unexpected for Element: " + locator);
+            }
+        } catch (Exception e) {
+            failureException = e.toString();
+            scenarioDef.log(Status.FAIL, errorMsg);
             throw e;
         }
     }
