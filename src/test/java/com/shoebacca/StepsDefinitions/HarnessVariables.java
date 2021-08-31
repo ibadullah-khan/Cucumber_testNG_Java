beb180d31dccc5fc43f6218e09984851a8512326
ZainAliQureshi <zain.ali.qureshi@hotmail.com>
2021-08-31 15:49:49 +0500

    C238460

--- src/test/java/com/shoebacca/StepsDefinitions/HarnessVariables.java
+++ src/test/java/com/shoebacca/StepsDefinitions/HarnessVariables.java
@@ -10,8 +10,8 @@ public class HarnessVariables extends UtilFactory {
     public HarnessVariables() throws Exception {
     }
     protected static String PRODUCT_NAME;
-    protected static String RELATED_PRODUCT_NAME;
     protected static String SHIPPING_METHOD;
+    protected static String SHIPPING_VALUE;
     protected static String[] ALL_EXPECTED_DATE;
     protected static String TAX_VALUE;
     protected static String PRODUCT_PRICE;
